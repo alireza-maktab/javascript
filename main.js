ef7d257de88aecdff24bbc4a1660c5f4dda7a8b1
Alireza <alirezamolaii@gmail.com>
2022-01-04 23:24:57 +0330

    add some example for find elements

---
+++ main.js
@@ -0,0 +1,49 @@
+// const grandparent = document.getElementById("khaan-daee");
+// changeColor(grandparent)
+
+// const parents = document.getElementsByClassName("parent");
+// const parentsArray = Array.from(parents);
+// parentsArray.forEach(changeColor)
+
+// const grandparent = document.querySelector("#khaan-daee");
+// changeColor(grandparent)
+
+// const grandparent = document.querySelector(".grandparent")
+// changeColor(grandparent)
+
+// const parent = document.querySelector(".parent");
+// changeColor(parent)
+
+// const parents = document.querySelectorAll(".parent");
+// parents.forEach(changeColor)
+
+// const grandparent = document.getElementById("khaan-daee");
+// const parents = grandparent.children
+// const parentsArray = Array.from(parents)
+// parentsArray.forEach(changeColor)
+// // const firstChild = parents[0].children[0]
+// // changeColor(firstChild)
+
+
+// const grandparent = document.getElementById("khaan-daee");
+// const firstParent = grandparent.children[0]
+// const firstChild = firstParent.querySelector(".child")
+// changeColor(firstChild)
+
+// const child = document.getElementById("abbas")
+// const parent = child.parentElement
+// changeColor(parent)
+// // changeColor(parent.parentElement)
+
+// const child = document.getElementById("abbas")
+// const grandparent = child.closest(".grandparent")
+// changeColor(grandparent)
+
+// const childOne = document.getElementById("abbas")
+// const childTow = childOne.nextElementSibling
+// changeColor(childTow)
+// // changeColor(childTow.previousElementSibling)
+
+function changeColor(element) {
+    element.style.backgroundColor = "#333"
+}
